30fc9d0bf7e4909510a4683dd60d0ef944d46a58
unknown <kcrayalgw@gmail.com>
2021-05-19 10:58:24 +0545

    home work

---
+++ Area.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+    {   double h,b,area;
+        cout << "Enter height";
+        cin >> h;
+        cout << "Enter base";
+        cin >> b;
+        area = 0.5*b*h;
+        cout << "\n The area is " << area;
+         /*int a;
+        cout << "Choose Method 1 or 2";
+        cin >> a;
+        if (a==1)
+        {
+            M1();
+        }
+        else
+        {
+            M2();
+        }*/
+        return 0;
+    }
+    /*
+void M1()
+    {
+        int a,b,c,s,area,x;
+        cout << "\n Enter 1st side \n";
+        cin >> a;
+        cout << " Enter 2nd side\n";
+        cin >> b;
+        cout << " Enter 3rd side\n";
+        cin >> c;
+        s=(a+b+c)/2;
+        cout << " \n The value of s is " << s <<endl ;
+        x=s*(s-a)*(s-b)*(s-c);
+        cout <<x ;
+        area = pow(x,0.5);
+        cout << "\n  The area is " << area;
+    }
+
+void M2()
+    [
+        int h,b,area
+        cout << "Enter height";
+        cin >> h;
+        cout << "Enter base";
+        cin >> b;
+        area = ( 1 / 2 ) * ( b * h );
+        cout << "\n The area is "<<area;
+
+    ] */
