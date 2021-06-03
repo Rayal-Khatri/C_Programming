621c6e2a30dc1ed4e2d591ec41fcea97fe29d095
unknown <kcrayalgw@gmail.com>
2021-06-03 12:18:05 +0545

    Simple calculator wuing switch case

--- Assessment/Assis_1/Q5_calculator.cpp
+++ Assessment/Assis_1/Q5_calculator.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
     {
-        //Q. Program to build a simple calculator using switch Statement
+        //Q. Program to build a simple calculator using switch Statement.
         int a,x,y,z;
         cout << "Main Menu: \n 1. Addition \n 2. Subtraction\n 3. Multiplication\n 4. Division ";
         cout << "\n Select from options: ";
