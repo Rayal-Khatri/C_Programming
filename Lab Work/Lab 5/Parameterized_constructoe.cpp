46540e6fcc0c0e24e01ec49b0440d4d0442b9e3d
unknown <kcrayalgw@gmail.com>
2021-07-11 16:15:35 +0545

    Parameterized  Constructor

--- Lab Work/Lab 5/Parameterized_constructoe.cpp
+++ Lab Work/Lab 5/Parameterized_constructoe.cpp
@@ -1,30 +1,52 @@
 #include<iostream>
+#include<string>
 using namespace std;
-class point
+class cars
 {
-    private:
-        int a,b;
-    public:
-        point(int x1,int y1)
-        {
-            a=x1;
-            b=y1;
-        }
-    int getx()
+private:
+string comp_name;
+string model_name;
+string fuel_name;
+float milage;
+float price;
+
+
+public: 
+//Default Constructor for C1
+cars()
     {
-        return a;
+        comp_name = "Toyota";
     }
-    int gety()
+    //parameterized constructor for C2
+cars(string mname, string ftype, float m,float p)
     {
-        return b;
+        model_name = mname;
+        fuel_name = ftype;
+        milage = m;
+        price = p;
+    }
+/*void setdata(string cname, string mname, string ftype, float m,float p)
+    {
+        comp_name = cname;
+        model_name = mname;
+        fuel_name = ftype;
+        milage = m;
+        price = p;
+    }*/
+void display()
+    {   cout<<"**************************"<<endl;
+        cout << model_name <<" :" << endl << endl;
+        cout << "Company : "<< comp_name << endl;
+        cout << "Fuel used : "<< fuel_name << endl;
+        cout << "Milage : "<< milage << endl;
+        cout << "Price : "<< price << endl;
     }
 };
-int main ()
-{
-    point p1(10,4);
-    cout << "p1.a = " << p1.getx() << " \np1.b = " << p1.gety() << endl;
-    return 0;
-}
 
-
-
+ int main()
+    {
+        cars c1,c2("Fortuner","Diseal", 20,120000); //everutime a object is created constructor is automatically called 
+        c1.display();//default constructor is called as it has no other value but the company name
+        c2.display();//cparameterized constructor is called as it doesnt have company name
+        return 0;
+    }
