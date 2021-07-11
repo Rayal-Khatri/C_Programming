72932c6644f2b2fb9b8e19cc7796758a39a81070
unknown <kcrayalgw@gmail.com>
2021-07-11 22:35:11 +0545

    Inheritance Simple example

---
+++ Lab Work/Lab6/Inheritance.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Shape
+{
+    protected:
+        int width;
+        int height;
+    public:
+    void setwidth(int w)
+    {
+       width=w;
+    }
+    void setheight(int h)
+    {
+        height=h;
+    }
+};
+class Rectangle:public Shape
+{
+        public:
+        int getArea()
+        {
+            return(width*height);
+        }
+};
+
+int main()
+{
+    Rectangle rect;
+    rect.setwidth(2);
+    rect.setheight(11);
+    cout<<"Total are is"<< rect.getArea()<<endl;
+    return 0;
+}
