a1103eba88b9d4adfc5ee598fef60a0722a09b87
unknown <kcrayalgw@gmail.com>
2021-07-11 16:05:28 +0545

    Default Constructor

--- Lab Work/Lab 5/Default_construct.cpp
+++ Lab Work/Lab 5/Default_construct.cpp
@@ -1,20 +1,50 @@
 #include<iostream>
+#include<string>
 using namespace std;
-class construct
+class cars
 {
-    public:
-    int a,b;
-    construct()
-        {
-            a=10;
-            b=20;
-        }
+private:
+string comp_name;
+string model_name;
+string fuel_name;
+float milage;
+float price;
+
+
+public: 
+//member functions
+cars()
+    {
+        cout <<"**************"<<endl; //to chek constructor is called or not
+        comp_name = "Toyota";
+        model_name = "Atlas";
+        fuel_name = "Petrol";
+        milage = 24;
+        price = 350000;
+    }
+/*void setdata(string cname, string mname, string ftype, float m,float p)
+    {
+        comp_name = cname;
+        model_name = mname;
+        fuel_name = ftype;
+        milage = m;
+        price = p;
+    }*/
+void display()
+    {  
+        cout << model_name <<" :" << endl << endl;
+        cout << "Company : "<< comp_name << endl;
+        cout << "Fuel used : "<< fuel_name << endl;
+        cout << "Milage : "<< milage << endl;
+        cout << "Price : "<< price << endl;
+    }
 };
 
-int main()
-{
-    construct c;
-    cout << "a is " << c.a << endl;
-    cout << "b is " << c.b << endl;
-    return 0;
-}
+ int main()
+    {
+        cars c1,c2; //everutime a object is created constructor is automatically called
+       // c1.setdata("Toyota","Altis","Petrol",25,350000);  
+        c1.display();//constructor gives given value if specified else some random value
+        c2.display();//constructor gives same value to it
+        return 0;
+    }
