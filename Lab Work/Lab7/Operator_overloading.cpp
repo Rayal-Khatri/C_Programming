3c5b444b9e75aeb381ed0b6842cf07fd903e15e5
unknown <kcrayalgw@gmail.com>
2021-07-12 15:25:22 +0545

    Operator overloading

---
+++ Lab Work/Lab7/Operator_overloading.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class complex
+{
+private:
+    int real;
+    int imaginary;
+public:
+complex()
+    {
+        real = 0;
+        imaginary=0;
+    }
+    complex(int r,int i)
+    {
+        real = r;
+        imaginary=i;
+    }
+    void print()
+    {
+        cout<<"The imaginary number is: "<< real<<" + "<<imaginary<<"i";
+    }
+    complex operator +(complex c)
+    {
+        complex temp;
+        temp.real=real+c.real;
+        temp.imaginary=imaginary +c.imaginary;
+        return temp;
+    }
+};
+int main()
+{
+    complex c1(2,3);
+    complex c2(3,4);
+    complex c3;
+    c3=c1+c2;
+    return 0;
+
+}
