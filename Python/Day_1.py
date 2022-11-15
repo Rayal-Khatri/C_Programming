8749ffd8b769df971eb85f250d610b05ada5154b
Rayal kc <kcrayal450@gmail.com>
2022-11-15 19:48:41 +0545

    Day1 programs

--- Python/Day_1.py
+++ Python/Day_1.py
@@ -16,15 +16,35 @@ c = 1.4           # This is a float
 b = a + int(str_1) + c
 print(f"The sum oof {a} and {int(str_1)} and {c} is", b)
 print(f"The boolean for condition {a}>{c} is", a>c)      #This is a Bool ie Returns True or False  
+
+#Complex Numbers
 comp_numb_1 = 3+4j
 comp_numb_2 = 5+8j    
 print("The sum of two complex number is ",comp_numb_1+comp_numb_2)
 
-# String Operations
+#Index in strings
+
 str=input("Enter a word: ")
+print('Indicate Index to print the specific character. Printing the index 3: ',str[3])
+print('Indicate Index to print the specific character. Printing the index -1: ',str[-1])
+print('Indicate Range of Index to print range of characters. Printing the index 0 to 3: ',str[0:3])
+print('Blank indicates start and end and another parameter indicates step value: ',str[::-1])
+print('Taking step value 2: ',str[::2])
+print("The length of the given word is", len(str))
+print(str.strip())  #Removes space from both ends (str.lstrip and str.rstrip for left and right only)
+str.find(input("Enter string to check Case"))                                            
+# String Operations
 print("Given string in uppercase is",str.upper())               #Upper & lower case
 print("Given string in lowercase is",str.lower())
 print("Given string after swaping case is",str.swapcase())      #Swaps case
+
+#Find and replace String
+para='''I am a good programmer and a quick brown fox jumps over a lazy dog. The world is a place to live and we should live in oeace and harmoy'''
+print("the location where the word dog is ",para.find('dog'))
+para.replace('fox','bear')                                    #One indicates how many times to replace
+print('after replacing fox with bear',para)
+print("The number of 'a' in the para is ", para.count('a'))                                                 #counts the no of given string in the word
+
 
 
 #Simple Program to change Temperature from C to F
