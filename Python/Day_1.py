1ddf8f1d95beeda81a145194c5f6eeb07b1b1006
Rayal kc <kcrayal450@gmail.com>
2022-11-15 13:12:31 +0545

    Python Day 1

--- Python/Day_1.py
+++ Python/Day_1.py
@@ -7,7 +7,7 @@ str2= "I am also a string"
 str3='''I am also a string but 
         I can 
         Do This'''
-print(a,b,c,str,str,str2,end='-') 
+print(a,b,c,str,str,str2,str3,sep='-')  #prints all the contend seperated by a -
 
 # Data Types
 a= 4            # This is a integer
@@ -17,4 +17,8 @@ b=a+ int(str_1)
 print(b)
 print(a>c)      #This is a Bool ie Returns True or False      
 
-#
+
+#Simple Program to change Temperature from C to F
+TempC=int(input("Enter Temperature in degree Celcius"))
+TempF = (TempC*9/5)+32
+print(f'{TempC} degrees in Celcius is {TempF} degrees in Farenheight')
