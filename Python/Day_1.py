2c7da6ab7fcfd1c1b4fe2e2604e97a52cf241b58
Rayal kc <kcrayal450@gmail.com>
2022-11-15 13:01:25 +0545

    Python Day 1

---
+++ Python/Day_1.py
@@ -0,0 +1,20 @@
+print('Hello World')
+# variables
+a=2
+b,c=4,5
+str='I am a test Program'
+str2= "I am also a string"
+str3='''I am also a string but 
+        I can 
+        Do This'''
+print(a,b,c,str,str,str2,end='-') 
+
+# Data Types
+a= 4            # This is a integer
+str_1='3'       # This is a string
+c=1.4           # This is a float
+b=a+ int(str_1)
+print(b)
+print(a>c)      #This is a Bool ie Returns True or False      
+
+#
