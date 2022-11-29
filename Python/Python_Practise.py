ddf44302e89e96943cd22f8da6cd5e3cc2b4213b
Rayal kc <kcrayal450@gmail.com>
2022-11-29 19:58:44 +0545

    Simple Program practise

---
+++ Python/Python_Practise.py
@@ -0,0 +1,12 @@
+username = input("Enter Username:")
+password = input("Enter Password:")
+print("Account Creation Succesful")
+print("Login :")
+username2 = input("Enter Username:")
+password2 = input("Enter Password:")
+if username == username2 and password==password2 :
+    pass
+else :  
+    print("Invalid Info provided")
+    k=input("Want to try again? (y/n)")
+    
